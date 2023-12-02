1971e4e7af6bb47910aade3100eb8ab53e28c495
Jonathan Moreno <mario_morenor@hotmail.com>
2023-12-01 19:24:37 -0500

    Mensaje de Confirmacion de Actualizacion de Usuario

--- src/app.js
+++ src/app.js
@@ -7,184 +7,192 @@ var listUserWin = null;
 var editUserWin = null;
 
 function createMenu() {
-    const templateMenu = [
+  const templateMenu = [
+    {
+      label: "Archivo",
+      submenu: [
+        { type: "separator" },
         {
-            label: "Archivo",
-            submenu: [
-                { type: 'separator' },
-                {
-                    label: "Salir",
-                    accelerator: "Ctrl+Q",
-                    click: () => {
-                        app.quit();
-                    }
-                }
-            ],
+          label: "Salir",
+          accelerator: "Ctrl+Q",
+          click: () => {
+            app.quit();
+          },
+        },
+      ],
+    },
+    {
+      label: "Usuarios",
+      submenu: [
+        {
+          label: "Nuevo",
+          accelerator: "Ctrl+N",
+          click: () => {
+            if (!createUserWin) {
+              createUserWin = new BrowserWindow({
+                width: 480,
+                height: 650,
+                webPreferences: {
+                  nodeIntegration: true,
+                  contextIsolation: false,
+                },
+              });
+              createUserWin.setMenu(null);
+              createUserWin.loadFile(
+                path.resolve(__dirname, "views", "createUser.html")
+              );
+
+              createUserWin.on("closed", () => {
+                createUserWin = null;
+              });
+            } else {
+              createUserWin.focus();
+            }
+          },
         },
         {
-            label: "Usuarios",
-            submenu: [
-                {
-                    label: "Nuevo",
-                    accelerator: "Ctrl+N",
-                    click: () => {
-
-                        if (!createUserWin) {
-                            createUserWin = new BrowserWindow({
-                                width: 480,
-                                height: 650,
-                                webPreferences: {
-                                    nodeIntegration: true,
-                                    contextIsolation: false
-                                }
-                            });
-                            createUserWin.setMenu(null)
-                            createUserWin.webContents.toggleDevTools()
-                            createUserWin.loadFile(path.resolve(__dirname, "views", "createUser.html"));
-
-                            createUserWin.on("closed", () => {
-                                createUserWin = null
-                            })
-                        } else {
-                            createUserWin.focus();
-                        }
-
-                    }
+          label: "Ver Usuarios",
+          accelerator: "Ctrl+L",
+          click: () => {
+            if (!listUserWin) {
+              listUserWin = new BrowserWindow({
+                width: 900,
+                height: 360,
+                webPreferences: {
+                  nodeIntegration: true,
+                  contextIsolation: false,
                 },
-                {
-                    label: "Ver Usuarios",
-                    accelerator: "Ctrl+L",
-                    click: () => {
+              });
+              listUserWin.setMenu(null);
+              listUserWin.loadFile(
+                path.resolve(__dirname, "views", "listUser.html")
+              );
 
-                        if (!listUserWin) {
-                            listUserWin = new BrowserWindow({
-                                width: 900,
-                                height: 360,
-                                webPreferences: {
-                                    nodeIntegration: true,
-                                    contextIsolation: false
-                                }
-                            });
-                            listUserWin.setMenu(null)
-                            listUserWin.webContents.toggleDevTools();
-                            listUserWin.loadFile(path.resolve(__dirname, "views", "listUser.html"));
+              listUserWin.on("closed", () => {
+                listUserWin = null;
+              });
+            } else {
+              listUserWin.focus();
+            }
+          },
+        },
+      ],
+    },
+  ];
 
-                            listUserWin.on("closed", () => {
-                                listUserWin = null
-                            })
-                        } else {
-                            listUserWin.focus();
-                        }
-                    }
-                }
-            ]
-        }
-    ];
-
-    const menu = Menu.buildFromTemplate(templateMenu);
-    Menu.setApplicationMenu(menu)
+  const menu = Menu.buildFromTemplate(templateMenu);
+  Menu.setApplicationMenu(menu);
 }
 
-
 app.whenReady().then(() => {
-
-    mainWin = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false
-        }
-    });
-    createMenu();
-    mainWin.webContents.toggleDevTools();
-    mainWin.loadFile(path.resolve(__dirname, "views", "index.html"))
-
+  mainWin = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+  createMenu();
+  mainWin.loadFile(path.resolve(__dirname, "views", "index.html"));
 });
 
+const DataStore = require("nedb");
 
-const DataStore = require('nedb');
+const users = new DataStore({ filename: "users", autoload: true });
 
-const users = new DataStore({ filename: 'users', autoload: true });
+ipcMain.on("create-user", (event, data) => {
+  users.insert(data);
+  createUserWin.close();
+});
 
+ipcMain.on("get-users", async (events, data) => {
+  users.find({}, (err, docs) => {
+    listUserWin.webContents.send("list-users", docs);
+  });
+});
 
-ipcMain.on('create-user', (event, data) => {
-    users.insert(data)
-    createUserWin.close();
-})
-
-ipcMain.on('get-users', async (events, data) => {
-    users.find({}, (err, docs) => {
-        listUserWin.webContents.send('list-users', docs)
-    });
-})
-
-
-ipcMain.on('delete-user', (event, data) => {
-    dialog.showMessageBox(listUserWin, {
-        message: 'Está a punto de eliminar un registro, Confirmar?',
-        buttons: ['Confirmo', 'Desconfirmo xD']
-    }).then(res => {
-        if (res.response == 0) {
-            users.remove({
-                _id: data
-            });
-
-            users.find({}, (err, docs) => {
-                listUserWin.webContents.send('list-users', docs)
-            });
-        }
+ipcMain.on("delete-user", (event, data) => {
+  dialog
+    .showMessageBox(listUserWin, {
+      message: "Está a punto de eliminar un registro, Confirmar?",
+      buttons: ["Desconfirmo xD", "Simón, Confirmo"],
+      noLink: true,
+      type: "warning",
+      title: "Eliminanding...",
     })
-
-
-})
-
-
-ipcMain.on('edit-user', (event, user_id) => {
-    if (!editUserWin) {
-        editUserWin = new BrowserWindow({
-            width: 480,
-            height: 650,
-            webPreferences: {
-                nodeIntegration: true,
-                contextIsolation: false
-            }
-        });
-        editUserWin.setMenu(null)
-        editUserWin.webContents.toggleDevTools();
-        editUserWin.loadFile(path.resolve(__dirname, "views", "editUser.html"));
-
-        editUserWin.on("closed", () => {
-            users.find({}, (err, docs) => {
-                listUserWin.webContents.send('list-users', docs)
-            });
-            editUserWin = null
+    .then((res) => {
+      if (res.response == 1) {
+        users.remove({
+          _id: data,
         });
 
-
-        ipcMain.on('show-user', (event) => {
-            users.findOne({
-                _id: user_id
-            }, (err, user) => {
-                editUserWin.webContents.send('show-user', user);
-            })
-        })
-
-    } else {
-        editUserWin.focus();
-    }
+        users.find({}, (err, docs) => {
+          listUserWin.webContents.send("list-users", docs);
+        });
+      }
+    });
 });
 
-ipcMain.on('update-user', (event, data) => {
+ipcMain.on("edit-user", (event, user_id) => {
+  if (!editUserWin) {
+    editUserWin = new BrowserWindow({
+      width: 480,
+      height: 650,
+      webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+      },
+    });
+    editUserWin.setMenu(null);
+    editUserWin.loadFile(path.resolve(__dirname, "views", "editUser.html"));
 
-    users.update({ _id: data.id }, {
-        $set: {
-            name: data.name,
-            last_name: data.last_name,
-            image: data.image,
-            dni: data.dni
-        }
+    editUserWin.on("closed", () => {
+      users.find({}, (err, docs) => {
+        listUserWin.webContents.send("list-users", docs);
+      });
+      editUserWin = null;
     });
 
-    editUserWin.close();
-})
+    ipcMain.on("show-user", (event) => {
+      users.findOne(
+        {
+          _id: user_id,
+        },
+        (err, user) => {
+          editUserWin.webContents.send("show-user", user);
+        }
+      );
+    });
+  } else {
+    editUserWin.focus();
+  }
+});
+
+ipcMain.on("update-user", (event, data) => {
+  dialog
+    .showMessageBox(editUserWin, {
+      message: "Está a punto de Modificar un registro, Confirmar?",
+      buttons: ["Desconfirmo xD", "Simón, Confirmo"],
+      noLink: true,
+      type: "info",
+      title: "Actualizando...",
+    })
+    .then((res) => {
+      if (res.response == 1) {
+        users.update(
+          { _id: data.id },
+          {
+            $set: {
+              name: data.name,
+              last_name: data.last_name,
+              image: data.image,
+              dni: data.dni,
+            },
+          }
+        );
+      }
+
+      editUserWin.close();
+    });
+});
